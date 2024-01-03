e2bb728d7f363303d891ac972f1946597c4b103e
danielhe27 <jdah49d@gmail.com>
2024-01-03 10:50:26 -0500

    first message lenght password

--- script.js
+++ script.js
@@ -1,6 +1,19 @@
 // Assignment Code
 const generateBtn = document.querySelector("#generate");
 
+function generatePassword(){
+// prompt to display a message to indicate the length of the key
+let length = parseInt(prompt("Enter the length of the password (between 8 and 128 characters):"));
+
+// indicates the lenght of the password  not less than 8 and not more than 128
+// inside of the loop we send a message to the client that needs a valid number inside of our range followed by the initial message
+while (isNaN(length) || length < 8 || length > 128) {
+alert("Please enter a valid number between 8 and 128.");
+length = parseInt(prompt("Enter the length of the password (between 8 and 128 characters):"));
+}
+
+
+
 
 // Write password to the #password input
 function writePassword() {
